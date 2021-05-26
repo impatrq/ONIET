514abdcdbca101215a65cb5f6d01bc19465f7e66
Fabrizio Carlassara <53102151+carlassaraf@users.noreply.github.com>
2021-05-26 20:32:16 -0300

    Create mario.py

---
+++ mario/mario.py
@@ -0,0 +1,12 @@
+# 
+# GitHub: @autor
+#
+# Para que la resolucion forme parte del repositorio:
+#
+#   - El programa debe estar apropiadamente comentado.
+#   - Los comentarios deben ser en linea (#).
+#   - Funciones propias deben ser documentadas con un docstring.
+#   - Guardar el archivo como mario_autor.py
+#
+
+# TODO
