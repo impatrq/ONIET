cd67f6c781988937f88b4ee9eec9ea92e295a27f
Fabrizio Carlassara <53102151+carlassaraf@users.noreply.github.com>
2021-05-26 19:59:52 -0300

    Update cajero.py

--- cajero/cajero.py
+++ cajero/cajero.py
@@ -1,8 +1,11 @@
-# Autor:
 # 
-# Consigna: ...
+# GitHub: @autor
 #
-#
+# Para que la resolucion forme parte del repositorio:
+#   - El programa debe estar apropiadamente comentado.
+#   - Los comentarios deben ser en linea (#).
+#   - Funciones propias deben ser documentadas con un docstring.
+#   - Guardar el archivo como cajero_autor.py
 #
 
 # TODO
