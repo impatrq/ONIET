eb08bc4577ceee8de733b8a7955371d2473ed337
lautipaletta <lautaropaletta1@gmail.com>
2021-10-02 10:47:43 -0300

    Clase 08 (En 07_auth) Agregada

--- Desarrollo de Sistemas/07_auth/todo/app/models.py
+++ Desarrollo de Sistemas/07_auth/todo/app/models.py
@@ -1,6 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+
+class User(AbstractUser):
+
+    username = models.CharField(
+        error_messages={
+            'unique': _("dadasda"),
+        },
+    )
+    colegio = models.CharField('Colegio', max_length=64, null=True, blank=True)
+    nacionalidad = models.CharField('Nacionalidad', max_length=64, null=True, blank=True, default='Argentina')
+
+    REQUIRED_FIELDS = ['first_name', 'last_name', 'email', 'colegio', ]
 
 class Task(models.Model):
 
